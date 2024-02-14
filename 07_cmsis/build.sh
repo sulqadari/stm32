488eda7f4b36ed2587de6e5cbef14572fc93c4aa
Telman Islamov <sulqadari@gmail.com>
2024-02-14 22:07:49 +0300

    Added ./src directory. All output files are stored in the build. folder.

--- 07_cmsis/build.sh
+++ 07_cmsis/build.sh
@@ -1,3 +1,10 @@
 #!bin/shell
-export CMSIS_STM32F1=/home/telman/develop/STM32CubeF1/Drivers/CMSIS/
+export CMSIS_STM32F1=~/develop/STM32_assembly/07_cmsis/CMSIS
+
+rm -rf build > /dev/null
+mkdir build && cd build
+
 make
+
+# cmake -GNinja ../
+# cmake --build .
